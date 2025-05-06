c94344325ba61b53fb8d6ed1c7d2a4a63d108912
Giulia Maria Castiglioni <giuliamaria.castiglioni@mail.polimi.it>
2025-05-06 14:15:01 +0000

    modified:   pages/Statistics.py

--- pages/Statistics.py
+++ pages/Statistics.py
@@ -5,10 +5,10 @@ import os
 # Carica e visualizza l'icona sopra il titolo
 #st.image("/workspaces/Formula-1-0/Assets/icons/Statistics.png", width=100)  # Puoi regolare la larghezza come preferisci
 
-st.title("Statistics")
+st.title("Statistics - Qualifing")
 
 st.write("""
-Dive into advanced race analytics: lap times, qualifying stats, pit stops, and driver comparisons.
+Dive into advanced race analytics: lap times, qualifying stats, and many more.
 """)
 
 def convert_time_to_seconds(time_str):
