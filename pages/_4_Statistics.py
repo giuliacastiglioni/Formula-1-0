67faa2fc0eabb91833a7acc2359e022be2b6c6b4
Giulia Maria Castiglioni <giuliamaria.castiglioni@mail.polimi.it>
2025-05-15 07:53:43 +0000

    modified:   pages/_1_Drivers.py 	modified:   pages/_4_Statistics.py

--- pages/_4_Statistics.py
+++ pages/_4_Statistics.py
@@ -5,7 +5,7 @@ import os
 # Carica e visualizza l'icona sopra il titolo
 #st.image("/workspaces/Formula-1-0/Assets/icons/Statistics.png", width=100)  # Puoi regolare la larghezza come preferisci
 
-st.title("Statistics - Qualifing")
+st.title("Qualifing")
 
 st.write("""
 Dive into advanced race analytics: lap times, qualifying stats, and many more.
